aea42f1b63871eb575829b61da8be144691e2596
eradesa <eranga.de.saram@gmail>
2026-04-19 08:28:57 +0530

    ini2

--- src/chess/server.py
+++ src/chess/server.py
@@ -2,7 +2,7 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP('Chess.com')
 
-from .chess_api import get_player_profile, get_player_stats
+from chess_api import get_player_profile, get_player_stats
 
 @mcp.tool()
 def get_chess_player_profile(username: str):
